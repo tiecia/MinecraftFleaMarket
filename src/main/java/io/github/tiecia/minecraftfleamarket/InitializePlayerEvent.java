fd8d5564ab018b729d44971e91edac33ce1e2d60
Tie C <usbtyping@gmail.com>
2020-05-22 16:16:05 -0700

    Fix log formatting

--- src/main/java/io/github/tiecia/minecraftfleamarket/InitializePlayerEvent.java
+++ src/main/java/io/github/tiecia/minecraftfleamarket/InitializePlayerEvent.java
@@ -22,7 +22,7 @@ public class InitializePlayerEvent implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         Map<UUID, Integer> bank = marketManager.getBank();
         if (!bank.keySet().contains(event.getPlayer().getUniqueId())) {
-            log(event.getPlayer().getDisplayName() + " was added to the system");
+            log(" " + event.getPlayer().getDisplayName() + " was added to the system");
             bank.put(event.getPlayer().getUniqueId(), 2000);
         }
         sendMessage(event.getPlayer(), "Current balance $" + bank.get(event.getPlayer().getUniqueId()));
